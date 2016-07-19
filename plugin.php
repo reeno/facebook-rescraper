cd44385fb2dec347ef497d2babcfb9446e19e9e8
reeno <nikolas.djuga+git@gmail.com>
2016-07-19 23:26:55 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,25 +1,36 @@
 <?php
 /*
-Plugin Name: Your Plugin Name
-Plugin URI: http://github.com/your_name/your_plugin
-Description: One line description of your plugin
-Version: 1.0
-Author: Your Name
-Author URI: http://your-site-if-any/
+Plugin Name: Facebook Scraper
+Plugin URI: http://your-own-domain-here.com/articles/hey-test-my-sample-plugin/
+Description: This plugin does something and something else
+Version: 0.1
+Author: Reeno
+Author URI: http://...
 */
+
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-/*
+yourls_add_action('post_add_new_link', 'reeno_facebook_scraper');
 
- Your code goes here.
- 
- Suggested read:
- https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
- https://github.com/YOURLS/YOURLS/wiki#for-developpers
- https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
- 
- Have fun!
- 
-*/
+function reeno_facebook_scraper($args) {
+	$access_token = 'APP_ID|APP_SECRET';
+
+	$params = array(
+		'id'	=> $args[0],
+		'scrape'=> 'true',
+		'access_token'	=> $access_token
+	);
+	
+	$ch = curl_init("https://graph.facebook.com");
+	curl_setopt_array($ch, array(
+		CURLOPT_RETURNTRANSFER=>true,
+		CURLOPT_SSL_VERIFYHOST=>false,
+		CURLOPT_SSL_VERIFYPEER=>false,
+		CURLOPT_POST=>true,
+		CURLOPT_POSTFIELDS=>$params
+	));
+	$result = curl_exec($ch);
+}
+
